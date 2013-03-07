8d228c5ce4f33069f282b466f695f31679e05b6a
Matt Gautreau <mgaut72@email.arizona.edu>
2013-03-07 15:12:54 -0700

    only opens the images once == more efficient

--- spriteMaker.py
+++ spriteMaker.py
@@ -18,10 +18,11 @@ import os
 max_width = 0
 total_height = 0
 
+imageList = [Image.open("./img/" + i) for i in os.listdir("./img/")]
+
 # loop through all the images in the folder to determine the dimensions
 # of our spritesheet
-for sprt in os.listdir("./img/"):
-    img = Image.open("./img/" + sprt)
+for img in imageList:
 
     if (max_width < img.size[0]):
         max_width = img.size[0]
@@ -40,8 +41,7 @@ fd = open('sprite_definitions.txt', 'w')
 
 
 # add each sprite to the total sheet
-for sprt in os.listdir("./img/"):
-    img = Image.open("./img/" + sprt)
+for img in imageList:
 
     fd.write(sprt + "\nUpper Left X: 0\nUpper Left Y: " + str(y_loc)
             + "\nWidth: " + str(img.size[0]) + "\nHeight: "
